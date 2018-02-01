124ed7738576e5a55be606c50f8d04d5c412e898
Stephen Vincent Mather <svm@clevelandmetroparks.com>
2018-02-01 17:18:56 +0000

    add the rest

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 41958807,
+    "boundingBox": {
+        "lx": 521585.921,
+        "ly": 9315543.25,
+        "lz": -80.8648,
+        "ux": 523479.8509999997,
+        "uy": 9317437.18,
+        "uz": 1813.065199999702
+    },
+    "tightBoundingBox": {
+        "lx": 521585.92,
+        "ly": 9315543.25,
+        "lz": -80.86,
+        "ux": 522967.25,
+        "uy": 9317437.18,
+        "uz": 161.82
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 16.401914596557618,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
